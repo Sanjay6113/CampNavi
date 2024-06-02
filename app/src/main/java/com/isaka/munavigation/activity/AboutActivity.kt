feaa181b16837fac4ad494fb393ea16066224b3d
Sanjay6113 <m.sanjay6113@gmail.com>
2024-06-02 20:34:04 +0530

    Initial commit

---
+++ app/src/main/java/com/isaka/munavigation/activity/AboutActivity.kt
@@ -0,0 +1,48 @@
+package com.isaka.munavigation.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import com.isaka.munavigation.BuildConfig
+import com.isaka.munavigation.databinding.ActivityAboutBinding
+
+class AboutActivity : BaseActivity() {
+    private lateinit var binding: ActivityAboutBinding
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // set app version
+        val appVersion = BuildConfig.VERSION_NAME
+        binding.tvAppVersion.text = "Version $appVersion"
+
+        // toolbar setup
+        setupActionBar()
+
+        binding.Continue.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+
+    }
+
+    // setup action bar
+    private fun setupActionBar() {
+        setSupportActionBar(binding.tbAbout)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = TOOL_BAR_TITLE
+
+        binding.tbAbout.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    companion object {
+        const val TOOL_BAR_TITLE = "About"
+    }
+}
